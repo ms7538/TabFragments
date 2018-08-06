5b4a0f9e5fa2003d18f95e5312704c819829c1b4
Marko <ms7538@gmail.com>
2018-08-06 04:27:34 -0600

    Initial commit

---
+++ app/src/main/java/com/poloapps/tabapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.poloapps.tabapplication;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HRFragment(), "Peak Heart Rates");
+        adapter.addFragment(new SpeedsFragment(), "Peak Speeds");
+        viewPager.setAdapter(adapter);
+    }
+
+}
